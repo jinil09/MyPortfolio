82489430a4546ae98312cb98283407e33a5aa931
Jinil Prajapati <109332175+jinil09@users.noreply.github.com>
2023-09-22 22:13:02 +0530

    Update main.js

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ $(document).ready(function(){
 
 
         if($(window).scrollTop() > 30){
-            $('.header').css({'background':'#6C5CE7', 'box-shadow':'0 .2rem .5rem rgba(0, 0, 0, .4)'});
+            $('.header').css({'background':'black', 'box-shadow':'0 .2rem .5rem rgba(0, 0, 0, .4)'});
         }
         else{
             $('.header').css({'background':'none', 'box-shadow':'none'}); 
@@ -19,3 +19,33 @@ $(document).ready(function(){
     });
 
 });
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    let fadeElements = document.querySelectorAll('.fade');
+  
+    function checkFadeElements() {
+      for (let i = 0; i < fadeElements.length; i++) {
+        let element = fadeElements[i];
+        let positionFromTop = element.getBoundingClientRect().top;
+  
+        if (positionFromTop - window.innerHeight <= 0) {
+          element.classList.add('show');
+        }
+      }
+    }
+  
+    window.addEventListener('scroll', checkFadeElements);
+    checkFadeElements();
+  });
+
+
+  // -------------------------------------------------------------------------------
+
+  const cursorElement = document.getElementById('cursor');
+        const cursorAnimationDuration = 4000; // Adjust the duration to match your typing animation
+
+        setTimeout(() => {
+            cursorElement.style.display = 'none';
+        }, cursorAnimationDuration);
+ 
